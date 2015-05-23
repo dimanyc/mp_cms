3e629a54bcdb96071b4e20d3b0d4ecd302d08fdc
dimanyc <dimitrynazarov@gmail.com>
2015-05-23 15:04:53 -0400

    removed extra animation

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -22,11 +22,12 @@ $(document).ready (function(){
 
   $( "#work-with-us,#work-with-us-footer" ).click(function() {
     $( "#contact-form:not(#contact-form h2)" ).css({opacity: 0.0, visibility: "visible"}).animate({opacity: 1.0},500);
-    $( "#contact-form h2" ).css({opacity: 0.0, visibility: "visible"}).delay(500).animate({opacity: 1.0},500);
-    $( "#contact-form span").each(function(i){
-       $(this).css({opacity: 0.0, visibility: "visible"}).delay(1000).delay(500*i).animate({opacity: 1.0},500);
-    });
-    $( "#contact-form form" ).css({opacity: 0.0, visibility: "visible"}).delay(2500).animate({opacity: 1.0},500);
+    $( "#contact-form h2,#contact-form span,#contact-form form" ).css({opacity: 0.0, visibility: "visible"}).delay(500).animate({opacity: 1.0},500);
+    // $( "#contact-form h2" ).css({opacity: 0.0, visibility: "visible"}).delay(500).animate({opacity: 1.0},500);
+    // $( "#contact-form span").each(function(i){
+    //    $(this).css({opacity: 0.0, visibility: "visible"}).delay(1000).delay(500*i).animate({opacity: 1.0},500);
+    // });
+    // $( "#contact-form form" ).css({opacity: 0.0, visibility: "visible"}).delay(2500).animate({opacity: 1.0},500);
 
   });
 
